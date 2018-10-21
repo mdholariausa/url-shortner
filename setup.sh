be97ae2f88d7f26dc60a9668b803ef35ea88d703
mdholariausa <mdholariausa@gmail.com>
2018-10-21 15:00:17 -0700

    - mysql installation - rake db create and migrate

--- setup.sh
+++ setup.sh
@@ -2,5 +2,15 @@
 
 rbenv init
 rbenv install "$(cat .ruby-version)"
+
+sudo sudo yum update -y
+sudo yum install mysql56
+
+sudo yum install mysql56-devel
+
+
 gem install bundle
 bundle install
+
+rake db:create
+rake db:migrate
